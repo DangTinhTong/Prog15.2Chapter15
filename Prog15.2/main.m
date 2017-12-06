60931879e986ab8af8f0b5e2cc402cd4607b4491
admin <dangtinhtong@gmail.com>
2017-12-06 23:23:45 +0700

    First commit

--- Prog15.2/main.m
+++ Prog15.2/main.m
@@ -4,14 +4,17 @@
 //
 //  Created by admin on 12/6/17.
 //  Copyright © 2017 admin. All rights reserved.
-//
+//String object
+
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        NSString *myString = @"Programming is fun.";
+        NSLog(@"%@",myString);
     }
     return 0;
 }
